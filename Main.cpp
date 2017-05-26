39b3793a04950ee2e8d4a74f0491dbda4d7e7563
OscarProgra3 <Daniel.embu96nitec.edu>
2017-05-26 15:47:28 -0600

    actualizando

--- Main.cpp
+++ Main.cpp
@@ -19,11 +19,20 @@ int main()
 	int resp=9;
 	while(resp!=3)
 	{
+		vector<Usuario*> listaUsuarios;
+
+		listaUsuarios(new Administrador(string, string, string, int, int, int, int, double, int, int));
+
+					listaobras.push_back(new Literatura(id,nombre,artista,fecha,generol,epoca));
+
 		cout<<endl<<endl<<"Bienvenido\n1.-Agregar\n2.-Entrar como administrador\n3.-Salir\nIngrese su opcion: ";
 		cin>>resp;
+
+
+
 		if (resp==1)//agregar
 		{
-			/* code */
+			cout<<endl<<""
 		}//finagregar
 		if (resp==2)//entrar administrador
 		{
