c42c3214b6280c50d710edd3ecc8743f0a61e94f
marvinestrada21 <marvinestrada2@gmail.com>
2017-05-26 16:40:54 -0600

    Pollo

--- Main.cpp
+++ Main.cpp
@@ -14,34 +14,10 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
 	int resp=9;
-	while(resp!=3)
-	{
-		vector<Usuario*> listaUsuarios;
-
-		listaUsuarios(new Administrador(string, string, string, int, int, int, int, double, int, int));
-
-					listaobras.push_back(new Literatura(id,nombre,artista,fecha,generol,epoca));
-
-		cout<<endl<<endl<<"Bienvenido\n1.-Agregar\n2.-Entrar como administrador\n3.-Salir\nIngrese su opcion: ";
-		cin>>resp;
-
-
-
-		if (resp==1)//agregar
-		{
-			cout<<endl<<""
-		}//finagregar
-		if (resp==2)//entrar administrador
-		{
-			/* code */
-		}//fin entrar administrador
-	}
-
+	vector<Usuario*> listaUsuarios;
 		
+	}
 	return 0;
-	
 }
-
